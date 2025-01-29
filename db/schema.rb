24a62947ca459b2dbb638b29f1dfdb280c453cbc
Patrick <patrickfeeneytamayo@gmail.com>
2025-01-29 00:06:16 +0000

    Add Registration and initiate session when registering

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_01_28_180726) do
+ActiveRecord::Schema[8.0].define(version: 2025_01_29_000008) do
   create_table "products", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -43,6 +43,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_01_28_180726) do
     t.string "password_digest", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
     t.index ["email_address"], name: "index_users_on_email_address", unique: true
   end
 
